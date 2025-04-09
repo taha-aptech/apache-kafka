b696cb416689fb44775afe075e7da8017f3c30a8
taha-aptech <taha_abbas@aptechnorth.edu.pk>
2025-04-09 16:00:39 +0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -6,7 +6,7 @@ producer = KafkaProducer(bootstrap_servers='localhost:6667')
 for i in range(10):
     message = 'Hello Kafka! Message {}'.format(i)
     producer.send('test_topic_python', message.encode('utf-8'))
-    print("Sent: {}".format(message))
+    print("Sent: {}".format(message))  # Replaced f-string with .format()
     time.sleep(1)
 
 producer.flush()
